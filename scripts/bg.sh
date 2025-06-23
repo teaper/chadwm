752a14491d0b2a2f6ebe8dbf831cf75bea9761c0
teaper <www@teaper.dev>
2025-06-23 20:47:23 +0800

    添加原神少女墙壁纸

--- scripts/bg.sh
+++ scripts/bg.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 while true 
 do 
-  num=$((RANDOM % 12 + 1))
-  img="/home/teaper/.config/chadwm/scripts/bgimgs/Travel$num.png"
+  num=$((RANDOM % 14 + 1))
+  img="/home/teaper/.config/chadwm/scripts/bgimgs/Tuya$num.png"
   feh --bg-fill "$img" 
   sleep 300 
 done
+
+
+
+
+
+#while true 
+#do 
+#  num=$((RANDOM % 12 + 1))
+#  img="/home/teaper/.config/chadwm/scripts/bgimgs/Travel$num.png"
+#  feh --bg-fill "$img" 
+#  sleep 300 
+#done
